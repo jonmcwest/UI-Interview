b8ba99c16a7c431f99119c985b37b88af6724f88
jonmac <jon.a.mccallum@gmail.com>
2024-12-05 04:35:02 +0000

    feat: add Top Trumps title and layout styling
    - Remove placeholder text from listing component
    - Add "TOP TRUMPS" title below Star Wars logo
    - Style main container and title with Orbitron font
    - Improve layout spacing and alignment

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-import './style.css'
-import starWarsLogo from '/logo.png'
-import { buildListing } from './listing.js'
+import './style.css';
+import starWarsLogo from '/logo.png';
+import { buildListing } from './listing.js';
 
 document.querySelector('#app').innerHTML = `
   <div class="bg"></div>
   <div class="main">
-    <a href="https://www.starwars.com" target="_blank">
-      <img src="${starWarsLogo}" class="logo" alt="Star Wars logo" />
-    </a>
+    <div class="title-container">
+      <a href="https://www.starwars.com" target="_blank">
+        <img src="${starWarsLogo}" class="logo" alt="Star Wars logo" />
+      </a>
+      <h2 class="title">TOP TRUMPS</h2>
+    </div>
     <ul id="listing"></ul>
   </div>
-`
-buildListing(document.querySelector('#listing')) //starting point!
+`;
+buildListing(document.querySelector('#listing')); //starting point!
